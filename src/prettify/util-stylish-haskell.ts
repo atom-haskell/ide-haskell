ff1ae0302529c57bb7d47bcca18321b8d163e7af
Nikolay Yakimov <root@livid.pp.ru>
2017-03-18 14:35:52 +0300

    Prettifier ts

--- src/prettify/util-stylish-haskell.ts
+++ src/prettify/util-stylish-haskell.ts
@@ -2,7 +2,7 @@
 
 import runFilter from './util-run-filter'
 
-export default function (text, workingDirectory) {
+export default function (text: string, workingDirectory: string) {
   return runFilter({
     command: atom.config.get('ide-haskell.stylishHaskellPath'),
     args: atom.config.get('ide-haskell.stylishHaskellArguments'),
