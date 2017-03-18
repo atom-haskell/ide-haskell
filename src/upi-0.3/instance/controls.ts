021d5b409280a3ab606fae9c4ce63350a06a5b4b
Nikolay Yakimov <root@livid.pp.ru>
2017-03-18 13:23:44 +0300

    Deconstruct UPI

---
+++ src/upi-0.3/instance/controls.ts
@@ -0,0 +1,41 @@
+import {CompositeDisposable, Point, Disposable, TextBuffer, TextEditor} from 'atom'
+import {PluginManager} from '../../plugin-manager'
+
+export interface IControlOpts {
+  id: string
+  events: {[key: string]: Function}
+  classes: string[]
+  style: {[key: string]: string}
+  attrs: {[key: string]: string}
+}
+export interface IUPIControlDefinition {
+  element: string | HTMLElement
+  opts: IControlOpts
+}
+export interface IMainInterface {
+  /**
+  Add a new control to ouptut panel heading.
+
+  element: HTMLElement of control, or String with tag name
+  opts: various options
+    id: String, id
+    events: Object, event callbacks, key is event name, e.g. "click",
+            value is callback
+    classes: Array of String, classes
+    style: Object, css style, keys are style attributes, values are values
+    attrs: Object, other attributes, keys are attribute names, values are values
+    before: String, CSS selector of element, that this one should be inserted
+            before, e.g. '#progressBar'
+
+  Returns Disposable.
+  */
+  add (def: IUPIControlDefinition): Disposable
+}
+
+export function create(pluginManager: PluginManager): IMainInterface {
+  return {
+    add ({element, opts}) {
+      return pluginManager.outputView.addPanelControl(element, opts)
+    }
+  }
+}
---
+++ src/upi-0.3/instance/tooltips.ts
@@ -0,0 +1,98 @@
+import {CompositeDisposable, Point, Disposable, TextBuffer, TextEditor} from 'atom'
+import {PluginManager} from '../../plugin-manager'
+import {UPI} from '../'
+import {UPIInstance} from './'
+import {TPosition, TUPIText, TEventRangeType} from './general'
+
+interface IShowTooltipParams {
+  editor: TextEditor
+  pos: TPosition
+  eventType: TEventRangeType
+  detail: any
+  tooltip: TTooltipFunction
+}
+type TTooltipFunction = (crange: Range) => ITooltipData | Promise<ITooltipData>
+interface ITooltipData {
+  range: Range
+  text: TUPIText
+  persistOnCursorMove?: boolean
+}
+export type TTooltipHandler = (editor: TextEditor, crange: Range, type: TEventRangeType) => ITooltipData | Promise<ITooltipData>
+
+export interface IMainInterface {
+  /**
+  Show tooltip in editor.
+
+  editor: editor that will show tooltip
+  pos: tooltip position
+  eventType: one of 'context', 'keyboard' and 'mouse'
+  detail: for automatic selection between 'context' and 'keyboard'.
+          Ignored if 'eventType' is set.
+  tooltip: function(crange)
+    crange: Range, currently selected range in editor (possibly empty)
+
+    Returns {range, text} or Promise
+      range: Range, tooltip highlighting range
+      persistOnCursorMove: Boolean, optional, default false, persist on cursor move regardless of settings
+      text: tooltip text. String or {text, highlighter} or {html}
+        text: tooltip text
+        highlighter: grammar scope that will be used to highlight tooltip text
+        html: html to be displayed in tooltip
+  */
+  show (params: IShowTooltipParams): void
+
+  /**
+  Editor event subscription. Fires when mouse cursor stopped over a symbol in
+  editor.
+
+  priority: event priority, higher value means higher priority,
+            subscription with hightest priority will be called first.
+  callback: callback(editor, crange, type)
+    editor: TextEditor, editor that generated event
+    crange: Range, cursor range that generated event.
+    type: One of 'mouse', 'selection' -- type of event that triggered this
+
+    Returns {range, text} or Promise.
+      range: Range, tooltip highlighting range
+      text: tooltip text. String or {text, highlighter} or {html}
+        text: tooltip text
+        highlighter: grammar scope that will be used to highlight tooltip text
+        html: html to be displayed in tooltip
+
+  returns Disposable
+  */
+  onShouldShowTooltip (priority: number, handler: TTooltipHandler): Disposable
+  onShouldShowTooltip (handler: TTooltipHandler): Disposable
+}
+
+export function create (pluginManager: PluginManager, main: UPI, instance: UPIInstance): IMainInterface {
+  return {
+    // TODO: merge this to UPI
+    show ({editor, pos, eventType, detail, tooltip}) {
+      const controller = pluginManager.controller(editor)
+      main.withEventRange({controller, pos, detail, eventType}, ({crange, pos}, eventType) => {
+        Promise.resolve(tooltip(crange)).then(({range, text, persistOnCursorMove}) =>
+          controller.showTooltip(pos, range, text, {eventType, subtype: 'external', persistOnCursorMove}))
+        .catch((status = {status: 'warning'}) => {
+          if (status.message) {
+            console.warn(status)
+            status = {status: 'warning'}
+          }
+          if (!status.ignore) {
+            controller.hideTooltip({eventType})
+            instance.messages.status(status)
+          }
+        })
+      })
+    },
+    onShouldShowTooltip (...args: any[]) {
+      if (args.length < 2) {
+        args.unshift(100)
+      }
+      const [priority, handler] = args
+      const obj = {priority, handler}
+      instance.tooltipEvents.add(obj)
+      return new Disposable(() => instance.tooltipEvents.delete(obj))
+    }
+  }
+}
