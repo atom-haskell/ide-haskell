1269931fdfe88759e3b0ceb5aecebd418c7a8703
Nikolay Yakimov <root@livid.pp.ru>
2017-07-16 14:54:24 +0300

    switching to subtree typings

--- typings/globals.d.ts
+++
@@ -1,7 +0,0 @@
-interface Function {
-  bind<T> (this: T, thisArg: any): T;
-}
-
-interface Array<T> {
-  map<U, V>(callbackfn: (value: T, index: number, array: T[]) => [U, V], thisArg?: any): [U, V][];
-}
