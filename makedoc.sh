382a60ed1da3db5c0762a71c781f4c3b6a8c5b80
Nikolay Yakimov <root@livid.pp.ru>
2017-07-18 23:32:40 +0300

    Fix docs (again)

--- makedoc.sh
+++ makedoc.sh
@@ -1,1 +1,1 @@
-node_modules/.bin/typedoc --entrypoint 'UPI' --out docs --includeDeclarations --ignoreCompilerErrors --mode file
+node_modules/.bin/typedoc --entrypoint UPI --out docs --includeDeclarations --ignoreCompilerErrors --mode file --name ide-haskell --readme none
