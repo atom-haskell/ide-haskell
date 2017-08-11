6ddd97ca775050ed42bb2d118d3d3fc7ceb320e3
Nikolay Yakimov <root@livid.pp.ru>
2017-08-11 15:11:36 +0300

    Resave after prettify
    Fixes #205

--- src/prettify/editor-controller.ts
+++ src/prettify/editor-controller.ts
@@ -9,6 +9,7 @@ export class PrettifyEditorController {
     return grammar.includes('haskell') || grammar.includes('cabal')
   }
   private disposables = new CompositeDisposable()
+  private isPretty: boolean = false
   constructor (private editor: TextEditor, pluginManager: PluginManager) {
     const buffer = this.editor.getBuffer()
     this.disposables.add(
@@ -20,13 +21,20 @@ export class PrettifyEditorController {
     this.disposables.dispose()
   }
 
-  private prettify () {
+  private async prettify () {
     if (atom.config.get('ide-haskell.onSavePrettify')) {
-      const scope = this.editor.getGrammar().scopeName
-      if (scope.includes('haskell')) {
-        prettifyFile(this.editor, 'haskell')
-      } else if (scope.includes('cabal')) {
-        prettifyFile(this.editor, 'cabal')
+      if (this.isPretty) { return }
+      this.isPretty = true
+      try {
+        const scope = this.editor.getGrammar().scopeName
+        if (scope.includes('haskell')) {
+          await prettifyFile(this.editor, 'haskell')
+        } else if (scope.includes('cabal')) {
+          await prettifyFile(this.editor, 'cabal')
+        }
+        await this.editor.getBuffer().save()
+      } finally {
+        this.isPretty = false
       }
     }
   }
