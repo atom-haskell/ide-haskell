183bd692fa7152470ddebd11451d2f27cd5203a3
Nikolay Yakimov <root@livid.pp.ru>
2018-01-01 04:18:34 +0300

    Add the most basic tests

---
+++ spec/package.spec.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai'
+import { join } from 'path'
+
+const pkg = join(__dirname, '..')
+
+describe('package', function () {
+  it('should activate', async () => {
+    const packages: any = atom.packages
+
+    // Load package, but it won't activate until the grammar is used
+    const promise = atom.packages.activatePackage(pkg)
+
+    packages.triggerActivationHook('language-haskell:grammar-used')
+    packages.triggerDeferredActivationHooks()
+
+    await promise
+
+    expect(atom.packages.isPackageActive('ide-haskell')).to.equal(true)
+  })
+})
